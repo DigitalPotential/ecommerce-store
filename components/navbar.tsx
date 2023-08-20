0695b8b3df0bc6c71eaf9fcdac634684267ca7f3
DigitalPotential <Carlos@digitalpotential.se>
2023-08-20 19:31:55 +0200

    feat: Navbar WIP

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,20 @@
+import Container from "@/components/ui/container";
+import Link from "next/link";
+import MainNav from "@/components/main-nav";
+
 const Navbar = () => {
     return (
         <div className="border-b">
-            navbar
+            <Container>
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+                    <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+                        <p className="font-bold text-xl">Store</p>
+                    </Link>
+                    <MainNav data={[]} />
+                </div>
+            </Container>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
