817a9ad6beb9d62f05223f74d88e3a01973a04d1
DigitalPotential <Carlos@digitalpotential.se>
2023-08-27 16:01:52 +0200

    feat: Cart Summary component

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -6,6 +6,7 @@ import Container from '@/components/ui/container';
 import useCart from '@/hooks/use-cart';
 
 import CartItem from './components/cart-item';
+import Summary from './components/summary';
 
 export const revalidate = 0;
 
@@ -35,6 +36,7 @@ const CartPage = () => {
                 ))}
               </ul>
             </div>
+            <Summary />
           </div>
         </div>
       </Container>
